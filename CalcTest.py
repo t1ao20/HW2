ea214b28c4481c93068025e0f6b7ec0e0fd8ea2a
t1ao20 <selina920120@gmail.com>
2025-10-05 03:16:51 +0800

    fix ModuleNotFoundError

--- CalcTest.py
+++ CalcTest.py
@@ -1,5 +1,5 @@
 import unittest
-from calculator import Calculator  # The class we are going to implement
+from Calc import Calculator  # The class we are going to implement
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
