7c98f4d04b3c0a50fdbecbb5a4e7e008209927e0
t1ao20 <selina920120@gmail.com>
2025-10-06 01:51:51 +0800

    add code coverage & static analysis

--- CalcTest.py
+++ CalcTest.py
@@ -1,6 +1,7 @@
 import unittest
 from Calc import Calculator  # The class we are going to implement
 
+
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         calc = Calculator()
@@ -21,13 +22,12 @@ class TestCalculator(unittest.TestCase):
         calc = Calculator()
         result = calc.div(10, 2)     # fix test case here
         self.assertEqual(result, 5)  # Expect 10 / 2 = 5
-    
+
     def test_div_by_zero(self):
         calc = Calculator()
         with self.assertRaises(ValueError):
             calc.div(10, 0)  # Division by zero should raise ValueError
 
+
 if __name__ == "__main__":
     unittest.main()
-
-    
